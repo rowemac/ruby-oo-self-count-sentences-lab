a0052d5bbba8257d50a37228bd2c14e6fd9bcaaf
Mac Rowe <m.rowe42@gmail.com>
2021-03-03 10:04:42 -0500

    commit tree beda98997492a5be2bb651b6dba30e811c399b44

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.split.last.include?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.split.last.include?("?")
+      true
+    else
+      false
+    end 
   end
 
   def exclamation?
-
+    if self.split.last.include?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #binding.pry
+    self.split(/[.!?]\s/).count
   end
 end
